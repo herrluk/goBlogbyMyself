14ea757a27718435f4e4b7b6469f66ec7abdfeb2
herrluk <herrluk@example.com>
2022-11-15 13:40:35 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+	"unicode/utf8"
+)
+
+var router = mux.NewRouter()
+var db *sql.DB
+
+// ArticlesFormData 创建博文表单数据
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func initDB() {
+	var err error
+	
+	config := mysql.Config{
+		User:                 "root",
+		Passwd:               "12345678",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "goblogByMyself",
+		AllowNativePasswords: true,
+	}
+	
+	// 准备数据库连接池
+	db, err := sql.Open("mysql", config.FormatDSN())
+	checkError(err)
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+	
+	// 尝试连接，失败会报错
+	err = db.Ping()
+	checkError(err)
+	
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello, 欢迎来到 管理系统！</h1>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+}
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID："+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "访问文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+	
+	errors := make(map[string]string)
+	
+	// 验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(
+		title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
+	}
+	
+	// 验证内容
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if len(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+	
+	// 检查是否有错误
+	if len(errors) == 0 {
+		lastInsertID, err := saveArticleToDB(title, body)
+		
+	} else {
+		
+		storeURL, _ := router.Get("articles.store").URL()
+		
+		data := ArticlesFormData{
+			title,
+			body,
+			storeURL,
+			errors,
+		}
+		
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+		if err != nil {
+			panic(err)
+			
+		}
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func saveArticleToDB(title string, body string) (int64, error) {
+	// 变量初始化
+	var (
+		id   int64
+		err  error
+		rs   sql.Result
+		stmt *sql.Stmt
+	)
+	
+	// 1. 获取一个 prepare 声明语句
+	stmt, err = db.Prepare("insert into articles(title, " +
+		"body) VALUES(? ,?)")
+	// 例行错误检测
+	if err != nil {
+		return 0, err
+	}
+	
+	// 2.在此函数运行结束后关闭此语句，防止占用 SQL 连接
+	defer stmt.Close()
+	
+	// 3.执行请求，传参进入绑定设备
+	rs, err = stmt.Exec(title, body)
+	if err != nil {
+		return 0, err
+	}
+	
+	// 4.插入成功的话，返回自增 ID
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+	return 0, err
+	
+}
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2. 继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		
+		// 2. 将请求传递下去
+		next.ServeHTTP(w, r)
+	})
+}
+
+func articlesCreateHandler(w http.ResponseWriter,
+	r *http.Request) {
+	
+	storeURL, _ := router.Get("articles.store").URL()
+	data := ArticlesFormData{
+		"",
+		"",
+		storeURL,
+		nil,
+	}
+	
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createTables() {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+	id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	body longtext COLLATE utf8mb4_unicode_ci
+); `
+	
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
+}
+func main() {
+	initDB()
+	createTables()
+	
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+	
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
+	
+	// 自定义 404 界面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	
+	// MiddleWare: force content type is HTML
+	router.Use(forceHTMLMiddleware)
+	
+	err := http.ListenAndServe(":3000", removeTrailingSlash(router))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
